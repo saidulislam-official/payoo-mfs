3eacb5288efe1e394dc04dcda9a97bfa24f97f3c
saidulislam-official <saidulislam.own@gmail.com>
2025-05-13 08:10:39 +0600

    added add money handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,24 @@
-console.log("home added");
+// add money to the account
+/* 
+    step-1 : add event handler
+    step-2 : prevent page reload after form submit
+    step-3 : get money to be added to the account balance
+    step-4 : get the pin number provided
+ */ 
+
+document.getElementById('btn-add-money')
+    .addEventListener('click',function(event){
+        // prevent page reload after form submit
+        event.preventDefault();
+        // console.log('money added button clicked');
+
+        // get money to be added to the account
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        console.log(addMoneyInput);
+
+        // get the pin number provided
+        const pinNumberInput = document.getElementById('input-pin-number').value;
+        console.log(pinNumberInput); 
+
+
+});
